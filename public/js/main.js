0410d40bc6ccbc643a78730b00e47e8cba249cca
Daniel Nass <dnlnss@gmail.com>
2017-04-29 13:24:19 -0400

    Improved start page rendering

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,49 @@
 const countdowns = {};
 
-const e = name => $('<' + name + '></' + name + '>');
+const e = name => $(`<${name}></${name}>`);
 
 $(document).ready(() => {
-  $.getJSON('/bots', bots => {
-    $.each(bots, function() {
-      $.getJSON('/bots/' + this, botData => renderBot(botData));
-    })
-  })
+  $.ajax({
+    type: 'POST',
+    url: '/bots'
+  }).done(botData => render(botData));
 })
 
 function getCount(lastRun, interval) {
   return moment("1900-01-01 00:00:00").add(moment(lastRun).add(interval, 'm').diff(moment()), 'milliseconds').format('HH:mm:ss').replace(/^(00:0?)+/, '');
 }
 
-function renderBot(botData) {
-  $.get('/views/bot.ejs', template => {
-    const id = botData.id;
-    const botDiv = ejs.compile(template)({bot: botData});
-    const $idDiv = $('#' + id);
-    if ($idDiv.length)
-      $idDiv.replaceWith(botDiv)
-    else
-      $('.bots').prepend(botDiv);
-    addHandlers(id);
-    if (botData.running) {
-      const interval = botData.interval;
-      const lastRun = botData.lastRun;
-      const $countdownDiv = $('#' + id + ' .countdown');
-      clearInterval(countdowns[id]);
-      $countdownDiv.text(getCount(lastRun, interval));
-      countdowns[id] = setInterval(() => {
+function render(botData) {
+  botData.forEach(bot => {
+    $.get('/views/bot.ejs', template => {
+      const id = bot.id;
+      const botDiv = ejs.compile(template)({ bot });
+      const $idDiv = $(`#${id}`);
+      if ($idDiv.length)
+        $idDiv.replaceWith(botDiv)
+      else
+        $('.bots').prepend(botDiv);
+      addHandlers(id);
+      if (bot.running) {
+        const interval = bot.interval;
+        const lastRun = bot.lastRun;
+        const $countdownDiv = $(`#${id} .countdown`);
+        clearInterval(countdowns[id]);
         $countdownDiv.text(getCount(lastRun, interval));
-        if (getCount(lastRun, interval) === '0:00') {
-          clearInterval(countdowns[id]);
-          setTimeout(() => {
-            handleResponse(JSON.stringify({id, action: 'render'}));
-          }, 5000);
-        }
-      }, 1000);
-    } else if (!botData.running && countdowns[id]) {
-      clearInterval(countdowns[id]);
-      countdowns[id] = null;
-    }
+        countdowns[id] = setInterval(() => {
+          $countdownDiv.text(getCount(lastRun, interval));
+          if (getCount(lastRun, interval) === '0:00') {
+            clearInterval(countdowns[id]);
+            setTimeout(() => {
+              handleResponse(JSON.stringify({id, action: 'render'}));
+            }, 5000);
+          }
+        }, 1000);
+      } else if (!bot.running && countdowns[id]) {
+        clearInterval(countdowns[id]);
+        countdowns[id] = null;
+      }
+    })
   })
 }
 
@@ -50,37 +51,44 @@ function handleResponse(resp) {
   const response = JSON.parse(resp);
   if (response.message) showMessage(response);
   if (response.id) {
-    if (response.action === 'delete') $('#' + response.id).remove();
-    else if (response.action === 'render') $.getJSON('/bots/' + response.id, botData => renderBot(botData));
+    const id = response.id;
+    if (response.action === 'delete') $(`#${id}`).remove();
+    else if (response.action === 'render') {
+      $.ajax({
+        type: 'POST',
+        url: '/bots',
+        data: { id }
+      }).done(botData => render(botData));
+    }
   }
 }
 
 function addHandlers(id) {
-  $('#' + id + ' .playpause').click(() => {
+  $(`#${id} .playpause`).click(() => {
     $.ajax({
       type: 'POST',
-      url: '/toggle/' + id
+      url: `/toggle/${id}`
     }).done(handleResponse)
   })
 
-  $('#' + id + ' .run').click(() => {
+  $(`#${id} .run`).click(() => {
     $.ajax({
       type: 'POST',
-      url: '/run/' + id
+      url: `/run/${id}`
     }).done(handleResponse)
   })
 
-  $('#' + id + ' .edit').click(() => {
+  $(`#${id} .edit`).click(() => {
     $.ajax({
       type: 'POST',
-      url: '/edit/' + id
+      url: `/edit/${id}`
     }).done(handleResponse)
   })
 
-  $('#' + id + ' .configure').submit(() => {
+  $(`#${id} .configure`).submit(() => {
     const data = {};
     const err = [];
-    $.each($('#' + id + ' .config'), function() {
+    $.each($(`#${id} .config`), function() {
       const value = $(this).val();
       const key = $(this).attr('name');
       if (value === '') {
@@ -100,15 +108,15 @@ function addHandlers(id) {
       $.ajax({
         data,
         type: 'POST',
-        url: '/update/' + id
+        url: `/update/${id}`
       }).done(handleResponse)
     }
   })
 }
 
 function showMessage(response) {
-  const $messageDiv = e('div').addClass('message ' + response.message.type);
-  const $span = e('span').text(response.id + ': ' + response.message.text);
+  const $messageDiv = e('div').addClass(`message ${response.message.type}`);
+  const $span = e('span').text(`${response.id}: ${response.message.text}`);
   const $closeBtn = e('i').addClass('fa fa-times-circle close').click(() => $messageDiv.remove())
   $messageDiv.append($span, $closeBtn);
   const $message = $('.message');
